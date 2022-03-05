acf31d91d24c5675f184c65d93b49e06ae1f35c0
hounandan <hounandanofs@gmail.com>
2022-03-06 01:13:41 +0530

    Boolean data type

--- Variables/src/Student.java
+++ Variables/src/Student.java
@@ -1,69 +1,41 @@
-
 public class Student {
 
 
-    public static void main(String[] args) {
+        byte rank = 123;
+        short id = 12345;
+        int number = 1234567890;
+        long phone = 7338880103L;
 
-        char myChar = 'A';
-        char charByInt = 65;
-        char charByUnicode = '\u0041';
+        boolean isInternationalStudent = true;
 
-        System.out.println("As Char = "+myChar);
-        System.out.println("As Int  = "+charByInt);
-        System.out.println("As Uni  = "+charByUnicode);
-
-        /*
-        We can use Integer Literals to define/initialize the character variable
-        But, Internally it is stored as "Unsigned integer"
-        Range : 0 to 65535
-
-        char myChar = -1 // Will throw error
-
-         */
-
-        char charByHex = 0x0041;
-        char charByOctal = 0101;
-        char charByBinary = 0b0100_0001;
-        System.out.println("As Hex  = "+charByHex);
-        System.out.println("As Octal = "+charByOctal);
-        System.out.println("As Binary = "+charByBinary);
-
-        /*
-        We can use also use the characters to define integer as below
-        Very Un-Common - but possible
-         */
-
-        int intChar = 'A';
-        System.out.println("Char as Int = "+intChar);
+        double tutionFee = 15000d;
+        double internationalTutionFee = 2000d;
 
 
-        /*
-        Please refer "unicode-table.com" to view the reserved character for alphabets
-        for all languages, emoji and symbols.
-         */
+        void compute(){
 
-        char myUnicode1 = '\u0BA8';
-        char myUnicode2 = '\u0BA9';
-        char myUnicode3 = '\u0BCD';
-        char myUnicode4 = '\u0BA4';
-        char myUnicode5 = '\u0BA9';
-        char myUnicode6 = '\u0BCD';
-        char myUnicode7 = '\u0BB9';
-        char myUnicode8 = '\u0B6D';
+                System.out.println("Rank : "+rank);
+                System.out.println("Id : "+id);
+                System.out.println("Number : "+number);
+                System.out.println("Phone : "+phone);
 
-        System.out.println();
-        System.out.print("Hounandan in Tamil  = ");
-        System.out.print(myUnicode7);
-        System.out.print(myUnicode8);
-        System.out.print(myUnicode1);
-        System.out.print(myUnicode2);
-        System.out.print(myUnicode3);
-        System.out.print(myUnicode4);
-        System.out.print(myUnicode5);
-        System.out.println(myUnicode6);
+                if (isInternationalStudent){
+                        tutionFee = tutionFee + internationalTutionFee;
+                }
+
+                System.out.println("Tuition Fee : "+tutionFee);
 
 
 
-    }
+        }
+
+
+        public static void main(String[] args) {
+
+                Student student = new Student();
+
+                student.compute();
+
+        }
 
 }
