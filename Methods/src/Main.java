c74a8e043c0aa91518541245103b04977e41481d
hounandan <hounandanofs@gmail.com>
2022-03-09 22:59:30 +0530

    Varargs method overloading

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -1,32 +1,23 @@
 public class Main {
 
 
-    static void go(int[] array){
-        System.out.println("array[0] : "+array[0]);
-        System.out.println("array[1] : "+array[1]);
-        array[1] = 22;
+    static void varargsMethod(boolean flag, int a, int b, int c){
+        System.out.println("Method without Varargs");
     }
 
-    static void go(int number){
-        System.out.println("go(int number)");
-    }
-
-    static void go(short s){
-        System.out.println("go(short s)");
+    static void varargsMethod(boolean flag, int ... i){
+        System.out.println("Method with Varargs");
+        System.out.println("Size of int array: "+i.length);
     }
 
 
     public static void main(String[] args) {
 
-        int[] array = {1,2};
-        go(array);
-        System.out.println("array[1] : "+array[1]);
+        Main.varargsMethod(true, 1, 2, 3);
+        Main.varargsMethod(true, 4, 5, 89, 7);
+        Main.varargsMethod(true, 1,2,3,4,5,6,7,8,9);
 
-        int number = 1000;
-        go(number);
 
-        short s = 20;
-        go(s);
     }
 
 }
