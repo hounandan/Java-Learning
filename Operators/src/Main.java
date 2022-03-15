316ad3bb8d38ba63ccb9e65cd9f361eff28a0afc
hounandan <hounandanofs@gmail.com>
2022-03-15 19:58:43 +0530

    Comparison Operator
    -- Main class and Student class
    -- Main class - preAndPost(), compoundArithmeticAssignments(), isOddOrEven()
    -- Student class - updateProfile() and update()

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -39,12 +39,34 @@ public class Main {
         System.out.println(a%2);
     }
 
+    static void comparisonOperators(){
+        int age = 25;
+
+        System.out.println("age > 21 : "+ (age>21));
+        System.out.println("age >= 21 : "+ (age>=21));
+        System.out.println("age < 21 : "+ (age<21));
+        System.out.println("age <= 21 : "+ (age<=21));
+        System.out.println("age == 21 : "+ (age==21));
+        System.out.println("age != 21 : "+ (age!=21));
+
+        Student s1 = new Student(1, "Nandan");
+        Student s2 = new Student(2, "Hounandan");
+        System.out.println("s1 == s2: "+ (s1 == s2));
+        System.out.println("s1 != s2: "+ (s1 != s2));
+
+        s1.update(s1, "Nandy");
+        System.out.println("Updated name of s1: "+s1.name);
+
+    }
+
 
     public static void main(String[] args) {
 
-        preAndPost();
-        compoundArithmeticAssignments();
-        isOddOrEven(5);
+//        preAndPost();
+//        compoundArithmeticAssignments();
+//        isOddOrEven(5);
+
+        comparisonOperators();
 
     }
 
