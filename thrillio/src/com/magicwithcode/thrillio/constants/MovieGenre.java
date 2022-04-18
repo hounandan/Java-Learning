ed802fe2cc3155c4d87fa26bd8d88d4c761857de
hounandan <hounandanofs@gmail.com>
2022-04-18 19:56:11 +0530

    Project Thrillo - Constant Exporting Classes
    -- com.magicwithcode.thrillio.entities
    -- Classes: User, Bookmark, Book, Movie, WebLink, UserBookMark
    
    -- Singleton classes - UserManager and BookmarkManager
    
    -- Constant classes: UserType, Gender, MovieGenre, BookGenre

---
+++ thrillio/src/com/magicwithcode/thrillio/constants/MovieGenre.java
@@ -0,0 +1,33 @@
+package com.magicwithcode.thrillio.constants;
+
+public class MovieGenre {
+
+    private MovieGenre(){}
+
+    public static final String CLASSICS = "classics";
+    public static final String DRAMA = "drama";
+    public static final String SCIFI_AND_FANTASY = "scifi and fantasy";
+    public static final String ACTION_AND_ADVENTURE = "action and adventure";
+    public static final String THRILLERS = "thrillers";
+    public static final String MUSIC_AND_MUSICALS = "music and musicals";
+    public static final String TELEVISION = "television";
+    public static final String HORROR = "horror";
+    public static final String SPECIAL_INTEREST = "special interest";
+    public static final String INDEPENDENT = "independent";
+    public static final String SPORTS_AND_FITNESS = "sports and fitness";
+    public static final String ANIME_AND_ANIMATION = "anime and animation";
+    public static final String GAY_AND_LESBIAN = "gay and lesbian";
+    public static final String CLASSIC_MOVIE_MUSICALS = "classic movie musicals";
+    public static final String FAITH_AND_SPIRITUALITY = "faith and spirituality";
+    public static final String FOREIGN_DRAMAS = "foreign dramas";
+    public static final String FOREIGN_ACTION_AND_ADVENTURE = "foreign action and adventure";
+    public static final String FOREIGN_THRILLERS = "foreign thrillers";
+    public static final String TV_SHOWS = "tv shows";
+    public static final String DRAMAS = "dramas";
+    public static final String ROMANTIC_MOVIES = "romatic movies";
+    public static final String COMEDIES = "comedies";
+    public static final String DOCUMENTARIES = "documentaries";
+    public static final String FOREIGN_MOVIES = "foreign movies";
+
+
+}
